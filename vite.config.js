8576176b4878c2754cb0c6e0ed626086485a7d44
Karim Adel <120580280+karimadel99@users.noreply.github.com>
2026-03-30 16:34:29 +0200

    build: path alias, chunk splitting, imagemin, darkMode, re-enable jsx-no-target-blank

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteImagemin({
+      webp: { quality: 80 },
+      optipng: { optimizationLevel: 5 },
+      mozjpeg: { quality: 80 },
+      svgo: { plugins: [{ name: 'removeViewBox', active: false }] },
+    }),
+  ],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          icons: ['react-icons'],
+        },
+      },
+    },
+  },
+});
